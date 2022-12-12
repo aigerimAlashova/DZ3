65bd8c1153472b85b6ecd6fa8fcd323ef38f76f1
aigerimAlashova <aigerimalasheva39@gmail.com>
2022-12-12 14:10:43 +0600

    DZ3

---
+++ main.js
@@ -0,0 +1,44 @@
+// let a =prompt("ВВедите первое число")
+// let b =prompt("ВВедите первое число")
+
+// function number(a, b){
+//     if( a < b) {
+//         alert(a + "меньше" + b)
+//     }else if (a > b) {
+//         alert(a + "больше" + b)
+//     }else if(a = b ) {
+//         alert(a + "равно" + b) 
+//     }else{
+//         console.log(".");
+//     }
+
+//     }
+// number(a, b)
+
+// let g = [1,2,3,4,5]
+// let f = [1,2,3,4,5,6]
+
+// function twoMass (g, f){
+//     if(g.length < f.length) {
+//         console.log(f.length);
+//     }else{
+//         console.log(".");
+//     }
+  
+// }
+// twoMass(g, f)
+
+
+
+let j = "Hello"
+function countChar(j) {
+   if(j.length){
+    console.log(j.length)
+   }else{
+    console.log(j);
+   }
+
+    }
+countChar(j)    
+
+    
